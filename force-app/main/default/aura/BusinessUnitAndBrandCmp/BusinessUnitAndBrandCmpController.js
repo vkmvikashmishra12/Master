d36f6e4cfd9e56c17ef868d7ab0531af6177fc2a
vkmvikashmishra12 <vkmvikashmishra12@gmail.com>
2025-09-12 11:30:31 +0530

    just for new chages

--- force-app/main/default/aura/BusinessUnitAndBrandCmp/BusinessUnitAndBrandCmpController.js
+++ force-app/main/default/aura/BusinessUnitAndBrandCmp/BusinessUnitAndBrandCmpController.js
@@ -2,7 +2,8 @@
 console.log()sdklfkff;
  vvnm vnvnv nn
  just updating to devleopment brancjh
-
+jdjdjhdhew
+Radha
 	getBusinessUnitRecord : function(component, event, helper) {		
          helper.getBusinessUnitRecord(component, event, helper);
 	},
